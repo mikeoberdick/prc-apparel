13804387d66d2b89d9a84a6caccaf86ad30fd30f
Mike Oberdick <mikeoberdick@gmail.com>
2017-06-29 08:57:19 -0400

    Tweaks to Shop CSS

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -86,7 +86,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<div class="response" id="mce-success-response" style="display:none"></div>
 			</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 			<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_72c444e517c000d0bc8dc2204_176610aa0f" tabindex="-1" value=""></div>
-			<div class="clear"><input type="submit" value="NOTIFY ME" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+			<div class="clear"><input type="submit" value="NOTIFY ME" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary"></div>
 			</div>
 			</form>
 			</div>
