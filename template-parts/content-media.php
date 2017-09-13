b9637411a90dbb469ae0cad70a8d56c64c1cdf97
Mike Oberdick <mikeoberdick@gmail.com>
2017-09-13 19:16:48 -0400

    Updated Media Page

--- template-parts/content-media.php
+++ template-parts/content-media.php
@@ -10,56 +10,56 @@
 
 <div class="container">
 	<div class = "row">
-		<div class="col-sm-12">
-			<h3>Images</h3>	
+		<div class="col-sm-12 mb-3">
+			<h3>Images</h3>
+			<hr>
 		</div>
 	</div>
-
 <?php $images = get_field('media_images');
-
-if( $images ): $i = 0; ?>
+if( $images ): ?>
         <?php foreach( $images as $image ): ?>
-        	<?php if($i % 3 == 0) { ?>
-	<div class="row mediaContent">
- <?php } ?>
-            <div class = "col-md-4">
-                <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
-                <h5 class = "mt-3 mb-3 text-center"><?php echo $image['title']; ?></h5>
-                <a href="<?php echo $image['url']; ?>" download><button role = 'button' class = 'btn btn-primary btn-lg'>Download Image</button></a>
-            </div>
-            <?php $i++;
-if($i != 0 && $i % 3 == 0) { ?> 
-	</div><!-- end of row -->
-<?php
-} ?>
+    <div class="row mb-3 mediaDownloads">
+    	<div class="col-sm-4">
+    		<img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
+    	</div>
+        <div class = "col-sm-3">
+        	<h5><?php echo $image['title']; ?></h5>
+        </div>
 
-        <?php endforeach; ?>
+        <div class="col-sm-5">
+        	<a href="<?php echo $image['url']; ?>"><button role = 'button' class = 'mr-2 btn btn-primary'>View Image</button></a>
+        	<a href="<?php echo $image['url']; ?>" download><button role = 'button' class = 'btn btn-primary'>Download Image</button></a>
+        </div>
+
+    </div><!-- end of row -->
+	<?php endforeach; ?>
 <?php endif; ?>
 
 <div class = "row">
-		<div class="col-sm-12">
-			<h3>Files</h3>	
-		</div>
+	<div class="col-sm-12 mb-3">
+		<h3>Files</h3>
+		<hr>
 	</div>
+</div>
 
 <?php $files = get_field('media_files');
-
-if( $files ): $i = 0; ?>
+if( $files ): ?>
         <?php foreach( $files as $file ): ?>
-        	<?php if($i % 3 == 0) { ?>
-	<div class="row mediaContent">
- <?php } ?>
-            <div class = "col-md-4">
-                <h5 class = "mt-3 mb-3 text-center"><?php echo $file['title']; ?></h5>
-                <a href="<?php echo $file['url']; ?>" download><button role = 'button' class = 'btn btn-primary btn-lg'>Download File</button></a>
-            </div>
-            <?php $i++;
-if($i != 0 && $i % 3 == 0) { ?> 
-	</div><!-- end of row -->
-<?php
-} ?>
+        	<div class="row mb-3 mediaDownloads">
+    	<div class="col-sm-4">
+    		<i class="fa fa-file-pdf-o" aria-hidden="true"></i>
+    	</div>
+        <div class = "col-sm-3">
+        	<h5><?php echo $file['title']; ?></h5>
+        </div>
 
-        <?php endforeach; ?>
+        <div class="col-sm-5">
+        	<a href="<?php echo $file['url']; ?>"><button role = 'button' class = 'mr-2 btn btn-primary'>View Image</button></a>
+        	<a href="<?php echo $file['url']; ?>" download><button role = 'button' class = 'btn btn-primary'>Download Image</button></a>
+        </div>
+    </div><!-- end of row -->
+
+	<?php endforeach; ?>
 <?php endif; ?>
 
 </div><!-- .container -->
